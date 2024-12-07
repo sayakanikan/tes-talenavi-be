2f6b307a7185a52558398246161c7e0819f5e79c
Irfansyah <111201911796@mhs.dinus.ac.id>
2024-12-07 14:53:31 +0700

    add cors config

---
+++ src/main/java/talenavi/tes_talenavi_be/configuration/WebConfig.java
@@ -0,0 +1,21 @@
+package talenavi.tes_talenavi_be.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+  @Value("${cors.allowedOrigins}")
+  private String allowedOrigins;
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+            .allowedOrigins(allowedOrigins)
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+  }
+}
